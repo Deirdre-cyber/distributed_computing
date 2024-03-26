7bac34e07fc87778750bb0ef1391b460ad0a54a9
Deirdre Laptop <dleeee333@gmail.com>
2024-03-26 12:13:32 +0000

    Updated using throws, try with resources

--- EchoClientHelper2.java
+++ EchoClientHelper2.java
@@ -4,64 +4,76 @@ import javax.net.ssl.SSLSocket;
 
 import java.io.*;
 
-/**
- * This class is a module which provides the application logic
- * for an Echo client using stream-mode socket.
- * 
- * @author M. L. Liu
- */
-
 public class EchoClientHelper2 {
 
    private MyStreamSocket mySocket;
    private InetAddress serverHost;
    private int serverPort;
 
-   EchoClientHelper2(String hostName,
-         String portNum) throws SocketException,
-         UnknownHostException, IOException {
+   EchoClientHelper2(String hostName, String portNum) throws IOException {
+      try {
+         this.serverHost = InetAddress.getByName(hostName);
+         this.serverPort = Integer.parseInt(portNum);
+         this.mySocket = new MyStreamSocket(this.serverHost, this.serverPort);
+         System.out.println("Connection request made");
+      } catch (UnknownHostException e) {
+         throw new IOException ("Unknown host: " + hostName + "\n" +e.getMessage());
+      } catch (SocketException e) {
+         throw new IOException ("Unable to create socket." + "\n" + e.getMessage());
+      }
+   }
 
-      this.serverHost = InetAddress.getByName(hostName);
-      this.serverPort = Integer.parseInt(portNum);
-      // Instantiates a stream-mode socket and wait for a connection.
-      this.mySocket = new MyStreamSocket(this.serverHost,
-            this.serverPort);
-      /**/ System.out.println("Connection request made");
-   } // end constructor
-
-   public String login(String credentials) throws SocketException, IOException {
+   public String login(String credentials) throws IOException {
       String login = "LOGIN " + credentials;
-      mySocket.sendMessage(login);
+
+      try{
+         mySocket.sendMessage(login);
       return mySocket.receiveMessage();
+      } catch (IOException e) {
+         return "Error loggin in: " + e.getMessage();
+      }
    }
 
-   public String sendMessage(String userMessage) throws SocketException, IOException {
-
+   public String sendMessage(String userMessage) throws IOException{
       String message = "UPLOAD " + userMessage;
-      mySocket.sendMessage(message);
-      return mySocket.receiveMessage();
+      
+      try {
+         mySocket.sendMessage(message);
+         return mySocket.receiveMessage();
+      } catch (IOException e) {
+         throw new IOException("Error sending message: " + e.getMessage());
+      }
    }
 
-   public String readMessage(String messageId) throws SocketException, IOException {
-
+   public String readMessage(String messageId) throws IOException {
       String message = "DOWNLOAD " + messageId;
-      mySocket.sendMessage(message);
-      return mySocket.receiveMessage();
+      try {
+         mySocket.sendMessage(message);
+         return mySocket.receiveMessage();
+      } catch (IOException e) {
+         throw new IOException("Error reading message: " + e.getMessage());
+      }
    }
 
-   public String readAllMessages() throws SocketException, IOException {
+   public String readAllMessages() throws IOException {
       String message = "DOWNLOAD_ALL";
-      mySocket.sendMessage(message);
-      return mySocket.receiveMessage();
+      try{
+         mySocket.sendMessage(message);
+         return mySocket.receiveMessage();
+      } catch (IOException e) {
+         throw new IOException("Error reading all messages: " + e.getMessage());
+      }
    }
 
    public String logout() throws IOException {
       String message = "LOGOUT";
-
-      mySocket.sendMessage(message);
-      String response = mySocket.receiveMessage();
-      mySocket.close();
-      return response;
-      
+      try{
+         mySocket.sendMessage(message);
+         String response = mySocket.receiveMessage();
+         mySocket.close();
+         return response;
+      } catch (IOException e) {
+         throw new IOException("Error logging out: " + e.getMessage());
+      }
    }
-} // end class
+}
