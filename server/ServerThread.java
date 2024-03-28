c711a722155355140a2e2b6b29706355337de41c
Deirdre Laptop <dleeee333@gmail.com>
2024-03-28 11:17:40 +0000

    Added exception handling and extra steps to ensure ssl connections handled

--- server/ServerThread.java
+++ server/ServerThread.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
+import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
 
@@ -30,39 +31,54 @@ class ServerThread implements Runnable {
          SSLSession sslSession = ((SSLSocket) myDataSocket.getSocket()).getSession();
          log.info("Protocol: " + sslSession.getProtocol());
          log.info("Host: " + sslSession.getPeerHost());
+         log.info("Cipher suite: " + sslSession.getCipherSuite());
+         log.info("Session ID: " + sslSession.getId());
 
-         while (!done) {
-            message = myDataSocket.receiveMessage();
-            System.out.println("Received: " + message);
+         if (sslSession.isValid()) {
+            while (!done) {
+               message = myDataSocket.receiveMessage();
+               System.out.println("Received: " + message);
 
-            if (message.startsWith("LOGIN")) {
-               String credentials = message.substring(6);
-               login(credentials);
+               if (message.startsWith("LOGIN")) {
+                  String credentials = message.substring(6);
+                  login(credentials);
 
-            } else if (message.startsWith("UPLOAD")) {
-               String userMessage = message.substring(7);
-               upload(userMessage);
+               } else if (message.startsWith("UPLOAD")) {
+                  String userMessage = message.substring(7);
+                  upload(userMessage);
 
-            } else if (message.startsWith("DOWNLOAD ")) {
-               String messageId = message.substring(9);
-               int id = Integer.parseInt(messageId);
-               download(id);
+               } else if (message.startsWith("DOWNLOAD ")) {
+                  String messageId = message.substring(9);
+                  int id = Integer.parseInt(messageId);
+                  download(id);
 
-            } else if (message.startsWith("DOWNLOAD_ALL")) {
-               downloadAll();
+               } else if (message.startsWith("DOWNLOAD_ALL")) {
+                  downloadAll();
 
-            } else if (message.startsWith("LOGOUT")){
-               logout();
-               done = true;
+               } else if (message.startsWith("LOGOUT")) {
+                  logout();
+                  done = true;
 
-            } else if (message.equals("QUIT")) {
-               quit();
-               done = true;
+               } else if (message.equals("QUIT")) {
+                  quit();
+                  done = true;
 
-            } else {
-               myDataSocket.sendMessage("Invalid command");
+               } else {
+                  myDataSocket.sendMessage("Invalid command");
+               }
             }
+         } else {
+            log.warning("SSL session is invalid");
+            myDataSocket.close();
          }
+      } catch (SSLHandshakeException e) {
+         log.warning("SSL Handshake exception: " + e.getMessage());
+         try {
+            myDataSocket.close();
+         } catch (IOException e1) {
+            log.severe("Error closing connection: " + e1.getMessage());
+         }
+
       } catch (Exception ex) {
          log.warning("Exception caught in thread: " + ex);
       }
@@ -75,7 +91,8 @@ class ServerThread implements Runnable {
 
          String[] parts = credentials.split(" ");
          if (parts.length != 2 || parts == null) {
-            message.append("| 103 Invalid credentials format |\n| Please try again               |\n| Check logs for details         |\n");
+            message.append(
+                  "| 103 Invalid credentials format |\n| Please try again               |\n| Check logs for details         |\n");
             message.append(BOTTOM_BORDER);
             myDataSocket.sendMessage(message.toString());
             log.warning("Invalid credentials format: " + credentials + "\nMust be 'username password'");
@@ -89,7 +106,8 @@ class ServerThread implements Runnable {
             message.append("| 101 Login successful          |\n|  Welcome to SMProtocol        |\n" + BOTTOM_BORDER);
             myDataSocket.sendMessage(message.toString());
          } else {
-            message.append("| 102 Login unsuccessful        |\n| Please try again              |\n| Check logs for details        |\n");
+            message.append(
+                  "| 102 Login unsuccessful        |\n| Please try again              |\n| Check logs for details        |\n");
             myDataSocket.sendMessage(message.toString() + BOTTOM_BORDER);
             log.warning("Login unsuccessful\nIncorrect credentials: " + credentials);
          }
@@ -117,7 +135,8 @@ class ServerThread implements Runnable {
          }
       } else {
          try {
-            message.append("| 202 Upload unsuccessful       |\n| Attempted to upload           |\n| empty message.                |\n" );
+            message.append(
+                  "| 202 Upload unsuccessful       |\n| Attempted to upload           |\n| empty message.                |\n");
             myDataSocket.sendMessage(message.toString() + BOTTOM_BORDER);
          } catch (IOException e) {
             log.severe("Error sending error message: " + e.getMessage());
@@ -130,7 +149,8 @@ class ServerThread implements Runnable {
       message.append(TOP_BORDER);
       try {
          if (messages.containsKey(id)) {
-            message.append("| 301 Download successful       |\n| Message ID: " + id + "               |\n " + messages.get(id) + " \n");
+            message.append("| 301 Download successful       |\n| Message ID: " + id + "               |\n "
+                  + messages.get(id) + " \n");
             myDataSocket.sendMessage(message.toString() + BOTTOM_BORDER);
          } else {
             message.append("| 302 Download unsuccessful     |\n| Message ID not found          |\n");
@@ -151,7 +171,7 @@ class ServerThread implements Runnable {
          } else {
             StringBuilder allMessages = new StringBuilder();
             for (int key : messages.keySet()) {
-               allMessages.append(" " +key + ": " + messages.get(key) + " \n");
+               allMessages.append(" " + key + ": " + messages.get(key) + " \n");
             }
             message.append("| 401 Download of all           |\n| messages successful           |\n");
             myDataSocket.sendMessage(message.toString() + "\n" + allMessages.toString() + BOTTOM_BORDER);
@@ -161,7 +181,7 @@ class ServerThread implements Runnable {
       }
    }
 
-   private void logout(){
+   private void logout() {
       StringBuilder message = new StringBuilder();
       message.append(TOP_BORDER);
       try {
@@ -176,7 +196,7 @@ class ServerThread implements Runnable {
       StringBuilder message = new StringBuilder();
       message.append(TOP_BORDER);
       try {
-         message.append("| 502 Program quit successfully |\n| Goodbye                       |\n"); 
+         message.append("| 502 Program quit successfully |\n| Goodbye                       |\n");
          myDataSocket.sendMessage(message.toString() + BOTTOM_BORDER);
       } catch (IOException e) {
          log.severe("Error sending quit message: " + e.getMessage());
